cb0feaff983c742039f80f8f3b737e4df5f072ea
sohanur28 <gsrahman28@gmail.com>
2023-06-05 15:27:10 +0600

    Dynamic Page title by Helmet

---
+++ src/pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,15 @@
+import { Helmet } from 'react-helmet-async';
+
+const Menu = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>Bistro Boss | Menu</title>
+            </Helmet>
+
+            <h2>Mneu</h2>
+        </div>
+    );
+};
+
+export default Menu;
